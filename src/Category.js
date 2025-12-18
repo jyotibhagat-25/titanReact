c9a57e9b6acbb3a9581c12037cb59d3b67aafb38
Jyoti Bhagat <jyotibhagat7079@gmail.com>
2025-12-18 21:24:01 +0530

    commit
    category section

--- src/Category.js
+++ src/Category.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 
 
 const Category = () => {
-  
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then((response) => {
+      console.log(response.data);
+      setProducts(response.data)
+    });
+
+  }, []);
+
   return (
     <div>
       <section>
         <Container>
           <Row>
 
-            
+            <Col>
+                {
+                  products.map((product,index)=>{
+                    return(
+                      <div>
+                        <p>{product.id}</p>
+                      </div>
+                    )
+                  })
+                }
+            </Col>
 
           </Row>
         </Container>
